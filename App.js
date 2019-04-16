df71d91d653cdeda7bfe4751b9610b1aefac3073
Roger <rdpender3@gmail.com>
2019-04-15 21:16:49 -0700

    added store to main app

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import { createAppContainer, createStackNavigator } from "react-navigation";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { apiMiddleware, reducer } from "./redux";
 import Apartments from "./src/Apartments.js";
 import Confirmation from "./src/Confirmation.js";
 
+// Create Redux store
+const store = createStore(reducer, {}, applyMiddleware(apiMiddleware));
+
+// Fetch movie data
+store.dispatch({ type: "GET_APARTMENT_DATA" });
+
 export default class App extends Component {
   render() {
-    return <AppContainer />;
+    return (
+      <Provider store={store}>
+        <AppContainer />;
+      </Provider>
+    );
   }
 }
 
